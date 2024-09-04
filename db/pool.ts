99381ca358e1e2859a1060c77eadbbb65a58890c
Walter Fritsche <wfritsche95@protonmail.com>
2024-09-04 11:18:49 -0500

    ran prettier

--- db/pool.ts
+++ db/pool.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
 
-const pool = new Pool({connectionString: process.env.DB_URI});
+const pool = new Pool({ connectionString: process.env.DB_URI });
 
 export default pool;
