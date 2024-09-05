aafec6758e5d73b75b1f789e5f1beebf93dec7f7
Walter Fritsche <wfritsche95@protonmail.com>
2024-09-05 12:00:41 -0500

    added passport local auth setup

--- app.ts
+++ app.ts
@@ -8,6 +8,15 @@ import http from "node:http";
 import HttpError from "./lib/HttpError";
 import path from "node:path";
 import indexRouter from "./routes/indexRouter";
+import session from "express-session";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import db from "./db/pool";
+import bcrypt from "bcryptjs";
+import pgSimple from "connect-pg-simple";
+import logger from "morgan";
+const pgSession = pgSimple(session);
+// const pgSession = require("connect-pg-simple")(session)
 
 const app = express();
 const PORT = parseInt(process.env.PORT || "3000");
@@ -18,11 +27,76 @@ app.set("port", PORT);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      secure: process.env.NODE_ENV === "production" ? true : false,
+      partitioned: true,
+      sameSite: "lax",
+    },
+    store: new pgSession({
+      pool: db,
+      createTableIfMissing: true,
+    }),
+  }),
+);
 
-app.get("/", indexRouter);
+passport.use(
+  new LocalStrategy.Strategy(async (username, password, done) => {
+    try {
+      const { rows } = await db.query(
+        `SELECT *
+      FROM users
+      WHERE username = $1
+      `,
+        [username],
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username or password" });
+      }
+      const passwordsMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordsMatch) {
+        return done(null, false, { message: "Incorrect username or password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+passport.serializeUser((user, done) => {
+  //@ts-ignore
+  done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await db.query(`SELECT * FROM users WHERE id = $1`, [id]);
+    const user = rows[0];
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  console.log("Current user ", req.user);
+  next();
+});
+
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use("*", function (req, res, next) {
+app.use(function (req, res, next) {
   const error = new HttpError("Page does not exist.", 404);
   next(error);
 });
